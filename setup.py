badbce34f211189659314e1419d81744072f96aa
Ryan Scott <rscott.72.h@gmail.com>
2022-12-02 19:03:54 -0500

    Uses sphinx for docs

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(
-    name="ck_slash_bot",
-    version="0.1.0",
-    description="Cool Kids Discord bot with full slash command support",
-    author="David LaClair, Ryan Scott",
-    packages=[],
-)
